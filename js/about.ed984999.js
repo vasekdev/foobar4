802ac5f76895e08e5d6ed9f6efae2ed5a750f728
vasekdev <vasek.dev@gmail.com>
2023-06-09 09:35:44 -0300

    deploy

---
+++ js/about.ed984999.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvanillaproject"]=self["webpackChunkvanillaproject"]||[]).push([[443],{833:function(n,a,t){t.r(a),t.d(a,{default:function(){return f}});var e=t(3396);const r={class:"about"},u=(0,e._)("h1",null,"This is an about page",-1),c=[u];function s(n,a){return(0,e.wg)(),(0,e.iD)("div",r,c)}var i=t(89);const l={},o=(0,i.Z)(l,[["render",s]]);var f=o}}]);
+//# sourceMappingURL=about.ed984999.js.map
